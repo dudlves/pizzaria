9ef1043023714ecbc6d86b7d31db35d9bce3b988
Maria Eduarda <dudlves17@gmail.com>
2022-03-08 17:10:38 -0300

    Finalizado Fase 5

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const porta = 3000;
 const PizzaRouter = require('./routes/PizzasRouter')
@@ -6,6 +7,9 @@ app.get('/', (req,res) => {
     res.send("Olá, visitante!")
     console.log("Servidor rodando na porta 3000")
 })
+app.set('view enrige','ejs')
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', PizzaRouter)
 
 app.listen(porta)
+
