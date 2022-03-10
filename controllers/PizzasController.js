7eb0654ff054d14c521657cfa361a023e371ea25
Maria Eduarda <dudlves17@gmail.com>
2022-03-10 16:05:14 -0300

    começando a faze 12

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,6 +1,27 @@
 const Pizzas = {
     listar: (req,res) =>{
-        res.render('pizzas.ejs')
+        res.render('pizzas.ejs',{pizzas})
+    },
+    mostrar: (req,res) =>{
+        let id = req.params.id;
+        let buscar = pizzas => {
+            if (pizzas.id == id){
+                return true;
+            }else{
+                false;
+            }
+        };
+        const pizza = pizzas.find(buscar)
+        res.render('pizza.ejs',{pizza})
+    },
+    buscar: (req,res) =>{
+        let trechoBuscado = req.query.busca;
+
+        
     }
 };
+
+const req = require('express/lib/request');
+const { bus } = require('nodemon/lib/utils');
+const pizzas = require('../database/Pizzas.json')
 module.exports = Pizzas
