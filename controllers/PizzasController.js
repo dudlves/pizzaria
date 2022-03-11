d9c1bba3df78f0a640e6d01fdca4174ac7bc5661
Maria Eduarda <dudlves17@gmail.com>
2022-03-10 23:11:09 -0300

    Terminado a fase 15

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,9 +1,25 @@
 const Pizzas = {
     listar: (req,res) =>{
-        res.render('pizzas.ejs',{pizzas})
+        res.render('pizzas.ejs',{pizzas,busca:''})
     },
     mostrar: (req,res) =>{
         let id = req.params.id;
+        const pizza = pizzas.find(buscar)
+        
+        res.render('pizza.ejs',{pizza, posicao})
+
+        let posicao = pizzas.indexOf (pizza)
+
+        let idProxima = null
+        if (posicao < pizzas.length - 1) {
+            idProxima = pizzas[posicao + 1].id
+        }
+
+        let idAnterior = null 
+        if (posicao > 0) {
+            idAnterior = pizzas[posicao - 1].id
+        }
+
         let buscar = pizzas => {
             if (pizzas.id == id){
                 return true;
@@ -11,7 +27,7 @@ const Pizzas = {
                 false;
             }
         };
-        const pizza = pizzas.find(buscar)
+        
         res.render('pizza.ejs',{pizza})
     },
     buscar: (req,res) =>{
